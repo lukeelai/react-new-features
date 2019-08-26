c3be28a24afa82e7cd66978e27360f669871ea4d
Luke Lai <LukeL@meyer.com>
2019-08-26 09:34:35 -0700

    state and effect hooks

--- src/index.js
+++ src/index.js
@@ -1,10 +1,141 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const NoteApp = () => {
+  const [notes, setNotes] = useState([]);
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+
+  const addNote = e => {
+    e.preventDefault();
+    setNotes([...notes, { title, body }]);
+    setTitle("");
+    setBody("");
+  };
+
+  const removeNote = title => {
+    setNotes(notes.filter(note => note.title !== title));
+  };
+
+  //component did mount
+  useEffect(() => {
+    console.log("this should run once");
+    const notesData = JSON.parse(localStorage.getItem("notes"));
+    if (notesData) {
+      setNotes(notesData);
+    }
+  }, []);
+
+  useEffect(() => {
+    console.log("useEffect");
+    document.title = title;
+    localStorage.setItem("notes", JSON.stringify(notes));
+  }, [notes]);
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      {notes.map(note => (
+        <Note key={note.title} note={note} removeNote={removeNote} />
+      ))}
+      <p>Add Note</p>
+      <form onSubmit={addNote}>
+        <input
+          value={title}
+          onChange={e => {
+            setTitle(e.target.value);
+          }}
+        ></input>
+        <textarea
+          value={body}
+          onChange={e => {
+            setBody(e.target.value);
+          }}
+        ></textarea>
+        <button>Add Note</button>
+      </form>
+    </div>
+  );
+};
+
+const Note = ({ note, removeNote }) => {
+  useEffect(() => {
+    console.log("note effect");
+
+    //component did unmount
+    return () => {
+      console.log("cleaning up effect");
+    };
+  }, []);
+
+  return (
+    <div key={note.title}>
+      <h3>{note.title}</h3>
+      <p>{note.body}</p>
+      <button
+        onClick={() => {
+          removeNote(note.title);
+        }}
+      >
+        x
+      </button>
+    </div>
+  );
+};
+// const App = props => {
+//   const [count, setCount] = useState(props.count);
+//   const [text, setText] = useState("");
+
+//   useEffect(() => {
+//     console.log("this should run once");
+//   },[]);
+
+//   useEffect(() => {
+//     console.log("useEffect");
+//     document.title = count;
+//   }, [count]);
+
+//   return (
+//     <div>
+//       <p>
+//         The current {text || "count"} is {count}
+//       </p>
+//       <button
+//         onClick={() => {
+//           setCount(count + 1);
+//         }}
+//       >
+//         +1
+//       </button>
+//       <button
+//         onClick={() => {
+//           setCount(count - 1);
+//         }}
+//       >
+//         -1
+//       </button>
+//       <button
+//         onClick={() => {
+//           setCount(props.count);
+//         }}
+//       >
+//         reset
+//       </button>
+//       <input
+//         value={text}
+//         onChange={e => {
+//           setText(e.target.value);
+//         }}
+//       ></input>
+//     </div>
+//   );
+// };
+// App.defaultProps = {
+//   count: 0
+// };
+
+ReactDOM.render(<NoteApp />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
